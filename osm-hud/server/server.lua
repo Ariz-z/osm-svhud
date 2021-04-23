3c2e0ee23f20758d4c9cf3cf7e9f4cd3d5ea66fb
OsmiumOP <71386574+OsmiumOP@users.noreply.github.com>
2021-04-23 13:32:14 +0530

    Update server.lua

--- osm-hud/server/server.lua
+++ osm-hud/server/server.lua
@@ -1,22 +1,7 @@
 QBCore = nil
 TriggerEvent('QBCore:GetObject', function(obj) QBCore = obj end)
 
-RegisterServerEvent('osmhealthui:save')
-AddEventHandler('osmhealthui:save', function(data)
-    MySQL.Async.execute('UPDATE hud SET data = @data', {
-		['@data'] = json.encode(data)
-	})
-end)
 
-QBCore.Functions.CreateCallback('osmhealthui:getOffsets', function(source, cb)
-    MySQL.Async.fetchAll('SELECT * FROM hud', {}, function(result)
-        if result ~= nil then
-            cb(result)
-        else
-            cb(nil)
-        end
-    end)
-end)
 
 QBCore.Commands.Add("cash", "Check je cash", {}, false, function(source, args)
 	local src = source
@@ -32,9 +17,10 @@ QBCore.Commands.Add("bank", "Money in Bank", {}, false, function(source, args)
     TriggerClientEvent('hud:client:ShowMoneyBank', source, "bank")
 end)
 
+ResetStress = false
+
 RegisterServerEvent("qb-hud:Server:UpdateStress")
 AddEventHandler('qb-hud:Server:UpdateStress', function(StressGain)
-
 	local src = source
     local Player = QBCore.Functions.GetPlayer(src)
     local newStress
@@ -52,25 +38,7 @@ AddEventHandler('qb-hud:Server:UpdateStress', function(StressGain)
             newStress = 100
         end
         Player.Functions.SetMetaData("stress", newStress)
-		-- TriggerClientEvent("hud:client:UpdateStress", src, newStress)
-	end
-end)
-
-RegisterServerEvent("qb-hud:Server:UpdateNeeds")
-AddEventHandler('qb-hud:Server:UpdateNeeds', function(Gain)
-
-	local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    local newHunger
-
-    if Player ~= nil then
-
-        newHunger =  Player.PlayerData.metadata["hunger"] - Gain
-        newThirst =  Player.PlayerData.metadata["thirst"] - Gain
-
-        Player.Functions.SetMetaData("hunger", newHunger)
-        Player.Functions.SetMetaData("thirst", newThirst)
-
+		TriggerClientEvent("hud:client:UpdateStress", src, newStress)
 	end
 end)
 
@@ -93,8 +61,8 @@ AddEventHandler('qb-hud:Server:GainStress', function(amount)
             newStress = 100
         end
         Player.Functions.SetMetaData("stress", newStress)
-     --   TriggerClientEvent("qb-hud:client:update:stress", src, newStress)
-        TriggerClientEvent('QBCore:Notify', src, 'Stress gekregen', 'primary', 1500)
+        TriggerClientEvent("hud:client:UpdateStress", src, newStress)
+        TriggerClientEvent('QBCore:Notify', src, 'Increased stress', 'primary', 1500)
 	end
 end)
 
@@ -117,26 +85,16 @@ AddEventHandler('qb-hud:Server:RelieveStress', function(amount)
             newStress = 100
         end
         Player.Functions.SetMetaData("stress", newStress)
-        TriggerClientEvent("qb-hud:client:update:stress", src, newStress)
-        TriggerClientEvent('QBCore:Notify', src, 'Stress Relieved')
+        TriggerClientEvent("hud:client:UpdateStress", src, newStress)
+        TriggerClientEvent('QBCore:Notify', src, 'Decreased stress')
 	end
 end)
 
-QBCore.Functions.CreateCallback('QBCore:HasMoney', function(source, cb, count)
-	local retval = false
-	local Player = QBCore.Functions.GetPlayer(source)
-	if Player ~= nil then 
-		if Player.Functions.RemoveMoney('cash', count, true) == true then
-			retval = true
-		end
-	end
-	
-	cb(retval)
-end)
+
 
 QBCore.Commands.Add("incstress", "Check je cash", {}, false, function(source, args)
 	local src = source
-	local xPlayer = QBCore.Functions.GetPlayer(src)
+	local Player = QBCore.Functions.GetPlayer(src)
 
     Player.Functions.SetMetaData("stress", 70)
     
